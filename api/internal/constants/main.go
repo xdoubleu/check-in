d3517fb90be2016566b5b84a8d6418eac01a67cb
Xander Warszawski <xander@xdoubleu.com>
2025-10-21 19:33:27 +0200

    Fix tests

--- api/internal/constants/main.go
+++ api/internal/constants/main.go
@@ -3,6 +3,7 @@ package constants
 import "github.com/XDoubleU/essentia/pkg/context"
 
 const DateFormat = "2006-01-02"
+const DayRangeFormat = "2006-01-02 15:04"
 const CSVFileNameFormat = "20060102150405"
 
 const UserContextKey = context.Key("user")
--- api/internal/services/locations.go
+++ api/internal/services/locations.go
@@ -75,18 +75,16 @@ func (service LocationService) GetCheckInsEntriesDay(
 		locationsTimeZoneMap[location.ID] = location.TimeZone
 	}
 
-	dateFormat := "2006-01-02 15:04"
-
 	g := grapher.New[int](
 		grapher.Cumulative,
 		grapher.PreviousValue,
-		dateFormat,
+		constants.DayRangeFormat,
 		time.Minute,
 	)
 	capacitiesGrapher := grapher.New[int](
 		grapher.Normal,
 		grapher.None,
-		dateFormat,
+		constants.DayRangeFormat,
 		time.Minute,
 	)
 
