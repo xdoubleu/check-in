08a859cb6246a3b203313cc2dc32655d99746fe9
Xander Warszawski <xander@xdoubleu.com>
2025-10-21 19:55:07 +0200

    Fix linter error

--- web/src/components/charts/RangeChart.tsx
+++ web/src/components/charts/RangeChart.tsx
@@ -118,7 +118,7 @@ export default function RangeChart({
           setStartDate={setStartDate}
           setEndDate={setEndDate}
         />
-        <br/>
+        <br />
         <NoDataFound />
       </>
     )
